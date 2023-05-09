7e7d754b2c517ce71d14c703a1b5cefa6357a6fb
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-09 14:53:17 -0700

    create a /profile endpoint for fetching account info

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../model/users');
 
 router.get('/', (req, res) => {
   res.json({ message: 'Welcome to the API!' });
 });
+
+router.get('/profile', async (req, res) => {
+    try {
+      const user = await userModel.findOne({ email: 'test@gmail.com' });
+      
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+        const userResponse = {
+        name: user.name,
+        email: user.email,
+        created_at: user.created_at,
+        updated_at: user.updated_at,
+      };
+  
+      res.json(userResponse);
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      res.status(500).json({ message: 'Error fetching user data' });
+    }
+  });
 
 module.exports = router;
 
