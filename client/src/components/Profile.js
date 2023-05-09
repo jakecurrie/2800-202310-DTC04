b086b1409ec068c607edd0e4061f1b60bf024344
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-09 14:54:09 -0700

    add Profile.js react component

---
+++ client/src/components/Profile.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const Profile = () => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/profile');
+        const data = await response.json();
+        setUser(data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <h2>Profile</h2>
+          <p>Name: {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>Created At: {new Date(user.created_at).toLocaleString()}</p>
+          <p>Updated At: {new Date(user.updated_at).toLocaleString()}</p>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Profile;
+
