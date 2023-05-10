6b2edc0bf477e0242351e4ea4e407f024aec6b33
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-09 21:11:45 -0700

    fix bug on import statement

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import Register from './components/Register';
 
