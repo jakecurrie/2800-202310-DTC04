c062127409ffd9875c0cb2a7e014b90f449fac0a
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-10 15:04:40 -0700

    add navbar to app component

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Profile from './components/Profile';
 import Register from './components/Register';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
+    <>
+      <Navbar />
       <Routes>
         <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={< Register/>} />
+        <Route path="/register" element={<Register />} />
       </Routes>
+    </>
   );
 }
 
 export default App;
+
 
 
 
