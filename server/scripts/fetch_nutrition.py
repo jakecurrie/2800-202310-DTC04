06af0d83e83226256edee9ab03c6894108ccd7e0
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-10 18:32:52 -0700

    add python error handling

--- server/scripts/fetch_nutrition.py
+++ server/scripts/fetch_nutrition.py
@@ -2,17 +2,24 @@ import openai
 import sys
 import dotenv
 import os
+import json
 
 dotenv.load_dotenv('../.env')
 openai.api_key = os.getenv('GPT_API_KEY')
-
 meal = sys.argv[1]
 
-response = openai.Completion.create(
-  engine="text-davinci-002",
-  prompt=f"Provide nutritional information for a typical serving of {meal} in json format.",
-  temperature=0.3,
-  max_tokens=200
-)
+try:
+    response = openai.Completion.create(
+      engine="text-davinci-002",
+      prompt=f"Provide nutritional information for a typical serving of {meal} in json format.",
+      temperature=0.3,
+      max_tokens=200
+    )
 
-print(response.choices[0].text.strip())
+    output = json.loads(response.choices[0].text.strip())
+    print(json.dumps(output))
+
+except Exception as e:
+    error_message = {"error": str(e)}
+    print(json.dumps(error_message))
+
