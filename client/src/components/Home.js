e9bf29539bf298c5b78123f39cf3acd873e90277
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-19 12:08:20 -0700

    add dummy data and organize dashboard

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
 import MacronutrientBreakdown from './MacronutrientBreakdown.js';
 import PersonalBestChart from './PersonalBest.js';
 import NutritionalIntakeTS from './NutritionalIntakeTS.js';
@@ -9,15 +10,28 @@ const HomePage = () => {
   const exerciseName = 'exerciseName';
 
   return (
-    <div className="homepage">
-      <h1>Welcome to Fitness Tracker</h1>
-      <div className="chart-container">
-        <MacronutrientBreakdown userId={userId} />
-        <PersonalBestChart userId={userId} exerciseName={exerciseName} />
-        <NutritionalIntakeTS userId={userId} />
-      </div>
-    </div>
+    <Container fluid className="mt-3">
+      <Row>
+        <Col xs={12} className="text-center mb-3">
+          <h1>Home</h1>
+          <Button variant="primary" className="mr-2">Diet</Button>
+          <Button variant="primary">Fitness</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} md={4}>
+          <MacronutrientBreakdown userId={userId} />
+        </Col>
+        <Col xs={12} md={4}>
+          <PersonalBestChart userId={userId} exerciseName={exerciseName} />
+        </Col>
+        <Col xs={12} md={4}>
+          <NutritionalIntakeTS userId={userId} />
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
 export default HomePage;
+
--- client/src/components/PersonalBest.js
+++ client/src/components/PersonalBest.js
@@ -1,40 +1,80 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import axios from 'axios';
 
-const PersonalBestChart = ({ userId, exerciseName }) => {
-  const [data, setData] = useState([]);
+const PersonalBestChart = () => {
+  const [selectedExercise, setSelectedExercise] = useState("Bench Press");
+  // Hardcoded dummy data
+  const data = {
+    "Bench Press": [
+      { date: new Date("2023-04-19").getTime(), personalBest: 100, currentWeight: 100 },
+      { date: new Date("2023-04-21").getTime(), personalBest: 100, currentWeight: 90 },
+      { date: new Date("2023-04-23").getTime(), personalBest: 100, currentWeight: 95 },
+      { date: new Date("2023-04-25").getTime(), personalBest: 100, currentWeight: 98 },
+      { date: new Date("2023-04-27").getTime(), personalBest: 105, currentWeight: 105 },
+    ],
+    "Squat": [
+      { date: new Date("2023-04-19").getTime(), personalBest: 120, currentWeight: 120 },
+      { date: new Date("2023-04-21").getTime(), personalBest: 120, currentWeight: 115 },
+      { date: new Date("2023-04-23").getTime(), personalBest: 120, currentWeight: 115 },
+      { date: new Date("2023-04-25").getTime(), personalBest: 120, currentWeight: 118 },
+      { date: new Date("2023-04-27").getTime(), personalBest: 125, currentWeight: 125 },
+    ],
+    "Deadlift": [
+      { date: new Date("2023-04-19").getTime(), personalBest: 150, currentWeight: 150 },
+      { date: new Date("2023-04-21").getTime(), personalBest: 150, currentWeight: 140 },
+      { date: new Date("2023-04-23").getTime(), personalBest: 150, currentWeight: 145 },
+      { date: new Date("2023-04-25").getTime(), personalBest: 150, currentWeight: 148 },
+      { date: new Date("2023-04-27").getTime(), personalBest: 155, currentWeight: 155 },
+    ],
+    "Overhead Press": [
+      { date: new Date("2023-04-19").getTime(), personalBest: 70, currentWeight: 70 },
+      { date: new Date("2023-04-21").getTime(), personalBest: 70, currentWeight: 65 },
+      { date: new Date("2023-04-23").getTime(), personalBest: 70, currentWeight: 67 },
+      { date: new Date("2023-04-25").getTime(), personalBest: 70, currentWeight: 68 },
+      { date: new Date("2023-04-27").getTime(), personalBest: 75, currentWeight: 75 },
+    ],
+    "Barbell Row": [
+      { date: new Date("2023-04-19").getTime(), personalBest: 85, currentWeight: 85 },
+      { date: new Date("2023-04-21").getTime(), personalBest: 85, currentWeight: 80 },
+      { date: new Date("2023-04-23").getTime(), personalBest: 85, currentWeight: 82 },
+      { date: new Date("2023-04-25").getTime(), personalBest: 85, currentWeight: 84 },
+      { date: new Date("2023-04-27").getTime(), personalBest: 90, currentWeight: 90 },
+    ],
+  };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await axios.get(`/personalBest/${userId}/${exerciseName}`);
-        setData(res.data);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-
-    fetchData();
-  }, [userId, exerciseName]);
+  const handleExerciseChange = (event) => {
+    setSelectedExercise(event.target.value);
+  };
 
   return (
-    <LineChart
-      width={500}
-      height={300}
-      data={data}
-      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="week" />
-      <YAxis />
-      <Tooltip />
-      <Legend />
-      <Line type="monotone" dataKey="personalBest" stroke="#8884d8" activeDot={{ r: 8 }} />
-      <Line type="monotone" dataKey="currentWeight" stroke="#82ca9d" />
-    </LineChart>
+    <>
+      <h2>{selectedExercise} Personal Best vs Current Weight</h2>
+      <select value={selectedExercise} onChange={handleExerciseChange}>
+        {Object.keys(data).map(exercise => (
+          <option key={exercise} value={exercise}>
+            {exercise}
+          </option>
+        ))}
+      </select>
+
+      <LineChart
+        width={500}
+        height={300}
+        data={data[selectedExercise]}
+        margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" scale="time" type="number" domain={['auto', 'auto']} tickFormatter={(unixTime) => new Date(unixTime).toLocaleDateString()} />
+        <YAxis />
+        <Tooltip labelFormatter={(unixTime) => new Date(unixTime).toLocaleDateString()} />
+        <Legend />
+        <Line type="monotone" dataKey="personalBest" stroke="#8884d8" activeDot={{ r: 8 }} />
+        <Line type="monotone" dataKey="currentWeight" stroke="#82ca9d" />
+      </LineChart>
+    </>
   );
 };
 
 export default PersonalBestChart;
+
 
