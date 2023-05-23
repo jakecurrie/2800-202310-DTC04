e2f795eca06e0197c363b61e65202162f6934e34
Ivan Cheng <ivan.cheng011099@gmail.com>
2023-05-23 12:39:55 -0700

    test

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 import MacronutrientBreakdown from './MacronutrientBreakdown.js';
 import PersonalBestChart from './PersonalBest.js';
 import NutritionalIntakeTS from './NutritionalIntakeTS.js';
-import Leaderboard from './Leaderboard.js';
 import '../style/Home.css';
 
 const HomePage = () => {
@@ -22,13 +21,13 @@ const HomePage = () => {
           <h2 className="home-card-h2">Fitness</h2>
           <p className="home-card-arrow">&#62;</p>
         </Link>
-        <Link to="/app/nutrition" className="fitness-cards">
-          <h2 className="fitness-card-h2">Diet</h2>
-          <span className="fitness-card-arrow">→</span>
+        <Link to="/app/nutrition" className="home-cards">
+          <h2 className="home-card-h2">Diet</h2>
+          <p className="home-card-arrow">&#62;</p>
         </Link>
-        <div className="fitness-cards">
-          <h2 className="fitness-card-h2">Macronutrient Breakdown</h2>
-          <div className="fitness-card-chart">
+        <div className="home-cards">
+          <h2 className="home-card-h2">Macronutrient Breakdown</h2>
+          <div className="home-card-chart">
             <MacronutrientBreakdown userId={userId} />
           </div>
         </div>
@@ -41,6 +40,70 @@ const HomePage = () => {
         <div className="home-cards">
           <h2 className="home-card-h2">Nutritional Intake Time Series</h2>
           <div className="home-card-chart">
+            <NutritionalIntakeTS userId={userId} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
+
+
+
+
+
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import MacronutrientBreakdown from './MacronutrientBreakdown.js';
+import PersonalBestChart from './PersonalBest.js';
+import NutritionalIntakeTS from './NutritionalIntakeTS.js';
+import Leaderboard from './Leaderboard.js';
+import '../style/Home.css';
+
+const HomePage = () => {
+  // Replace 'userId' and 'exerciseName' with actual values or state
+  const userId = 'userId';
+  const exerciseName = 'exerciseName';
+
+  return (
+    <div id="fitness-body-container">
+      <div id="fitness-title-container">
+        <h1 id="fitness-title-h1">Home</h1>
+        <p id="fitness-title-welcome">Track your nutrition and workout progress</p>
+      </div>
+      <div id="fitness-card-container">
+        {/* <Link to="/app/fitness" className="fitness-cards">
+          <h2 className="fitness-card-h2">Fitness</h2>
+          <span className="fitness-card-arrow">→</span>
+        </Link>
+        <Link to="/app/nutrition" className="fitness-cards">
+          <h2 className="fitness-card-h2">Diet</h2>
+          <span className="fitness-card-arrow">→</span>
+        </Link> */}
+        <div className="fitness-cards">
+          <h2 className="fitness-card-h2">Leaderboards</h2>
+          <div className="fitness-card-chart">
+            <Leaderboard />
+          </div>
+        </div>
+        <div className="fitness-cards">
+          <h2 className="fitness-card-h2">Macronutrient Breakdown</h2>
+          <div className="fitness-card-chart">
+            <MacronutrientBreakdown userId={userId} />
+          </div>
+        </div>
+        <div className="fitness-cards">
+          <h2 className="fitness-card-h2">Personal Best</h2>
+          <div className="fitness-card-chart">
+            <PersonalBestChart userId={userId} exerciseName={exerciseName} />
+          </div>
+        </div>
+        <div className="fitness-cards">
+          <h2 className="fitness-card-h2">Nutritional Intake Time Series</h2>
+          <div className="fitness-card-chart">
             <NutritionalIntakeTS userId={userId} />
           </div>
         </div>
