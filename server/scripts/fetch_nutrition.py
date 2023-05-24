1a752fa83966b00e7de9bd3dd1338c53ab9c6c36
Jacob Currie <jcurrie42@my.bcit.ca>
2023-05-24 13:07:31 -0700

    add meal size paramater and adjust prompt

--- server/scripts/fetch_nutrition.py
+++ server/scripts/fetch_nutrition.py
@@ -9,13 +9,14 @@ load_dotenv()
 openai.api_key = os.getenv('GPT_API_KEY')
 
 meal = sys.argv[1]
+meal_size = sys.argv[2]
 
 try:
     response = openai.Completion.create(
       engine="text-davinci-002",
-      prompt=f"Provide nutritional information for a typical serving of {meal} in json format.",
+      prompt=f"Provide nutritional information for a {meal_size} serving of {meal} in json format, including details for calories (kcal), total fat (g), saturated fat (g), trans fat (g), cholesterol (mg), sodium (mg), total carbohydrate (g), dietary fiber (g), sugars (g), protein (g), vitamin D (μg), calcium (mg), iron (mg), and potassium (mg).",
       temperature=0.3,
-      max_tokens=200
+      max_tokens=300
     )
 
     output = json.loads(response.choices[0].text.strip())
@@ -24,5 +25,4 @@ try:
 except Exception as e:
     error_message = {"error": str(e)}
     print(json.dumps(error_message))
-
-
+    
