aa97c21634c4c12bc47d912c0ecceb3bfee414a0
vishav-josan <122079283+vishav-josan@users.noreply.github.com>
2023-05-25 09:20:27 -0700

    fix users to include points

--- server/routes/users.js
+++ server/routes/users.js
@@ -25,4 +25,34 @@ router.get('/currentPoint', async (req, res) => {
     }
 })
 
+router.get('/nearbyUsers', async (req, res) => {
+    try {
+        const users = await userModel.find().sort({ points: -1 }).exec();
+        const currentUserIndex = users.findIndex(user => user._id.toString() === req.session.USER_ID);
+        let start, end;
+        
+        if (currentUserIndex === 0 || currentUserIndex === 1) {
+          // When the current user is the first or second, display top 5 users
+          start = 0;
+          end = Math.min(users.length, 5);
+        } else if (currentUserIndex === users.length - 1 || currentUserIndex === users.length - 2) {
+          // When the current user is the last or second last, display last 5 users
+          start = Math.max(0, users.length - 5);
+          end = users.length;
+        } else {
+          // Otherwise, display the current user and two users ahead and behind
+          start = currentUserIndex - 2;
+          end = currentUserIndex + 3;
+        }
+
+        const aroundMeUsers = users.slice(start, end);
+        res.json(aroundMeUsers);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+
+
 module.exports = router;
